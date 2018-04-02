abf724940179a9775a8f7148e847c1117849702e
David Moreau Simard <dmsimard@redhat.com>
2018-04-02 10:12:22 -0400

    Tabs instead of spaces. Wow.
    Let's not do that.

--- benchmark.sh
+++ benchmark.sh
@@ -2,8 +2,8 @@
 releases="ubuntu-xenial fedora-27 centos-7"
 for release in $releases
 do
-	mkdir -p $release/results
-	log="$(uname -r)_permissive_$(date +%s).log"
-	docker run -it dmsimard/oslo.rootwrap-benchmark:$release |tee -a $release/results/$log
-	sleep 30
+    mkdir -p $release/results
+    log="$(uname -r)_permissive_$(date +%s).log"
+    docker run -it dmsimard/oslo.rootwrap-benchmark:$release |tee -a $release/results/$log
+    sleep 30
 done
